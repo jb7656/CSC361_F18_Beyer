3ceea903f47fa1e6e132b8c08c47bcf0dbd35b81
Jeff <jeffreyb223@gmail.com>
2018-10-31 14:02:10 -0400

    Implemented GameLoop/TestSprites

--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 import util.Constants;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 /**
  * Class for handling the graphics side of the game loop
@@ -17,14 +18,45 @@ public class WorldRenderer implements Disposable
 	private WorldController worldController;
 	
 	public WorldRenderer (WorldController worldController) 
-	{ }
+	{ 
+		this.worldController = worldController;
+		init();
+	}
 	
-	private void init () { }
+	private void init () 
+	{ 
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH,
+		Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0, 0, 0);
+		camera.update();
+	}
 	
-	public void render () { }
+	public void render () 
+	{ 
+		renderTestObjects();
+	}
 	
-	public void resize (int width, int height) { }
+	public void resize (int width, int height) 
+	{ 
+		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) *width;
+		camera.update();
+	}
 	
-	@Override public void dispose () { }
+	@Override public void dispose () 
+	{ 
+		batch.dispose();
+	}
+	
+	private void renderTestObjects() 
+	{
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		for(Sprite sprite : worldController.testSprites) 
+		{
+			sprite.draw(batch);
+		}
+		batch.end();
+	}
 }
 
