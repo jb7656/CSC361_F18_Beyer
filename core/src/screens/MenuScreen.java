b1c964d218d1ad082313f8aaf72be2faa958243d
Jeff <jeffreyb223@gmail.com>
2018-11-28 11:29:29 -0500

    Added main logo to assets and print it on menuscreen

--- core/src/screens/MenuScreen.java
+++ core/src/screens/MenuScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import util.CameraHelper;
@@ -15,10 +16,13 @@ public class MenuScreen extends AbstractGameScreen
 {
   private static final String TAG = MenuScreen.class.getName();
   private SpriteBatch batch;
-  Pixmap px= new Pixmap(Gdx.files.internal("menu_background.png"));
+  Pixmap px1= new Pixmap(Gdx.files.internal("menu_background.png"));
+  Pixmap px2 = new Pixmap(Gdx.files.internal("title_logo.png"));
   private OrthographicCamera camera;
   CameraHelper camhelper;
-  Texture background1 = new Texture(px);
+  Texture background1 = new Texture(px1);
+  Texture logo = new Texture(px2);
+  //BitmapFont font = new BitmapFont(Gdx.files.internal("data/rayanfont.fnt"), false);
   
   public MenuScreen (Game game) 
   {
@@ -27,7 +31,8 @@ public class MenuScreen extends AbstractGameScreen
     camhelper.setPosition(0, 0);
     batch = new SpriteBatch();
 	camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH,Constants.VIEWPORT_HEIGHT);
-	camera.position.set(0, 0,0);
+	camera.position.set(0,0,0);
+	//font.getData().setScale(.1f);
     
   }
   @Override
@@ -39,6 +44,8 @@ public class MenuScreen extends AbstractGameScreen
 	batch.setProjectionMatrix(camera.combined);
     	batch.begin();
     		batch.draw(background1,-2.5f, -2.5f, 5.5f, 5.0f);
+    		batch.draw(logo,-1.5f, -1.75f, 3.5f, 3.5f);
+    		//font.draw(batch, "swim game", -2, 2);
     	batch.end();
     if(Gdx.input.isTouched())
       game.setScreen(new GameScreen(game));
